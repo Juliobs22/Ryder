59d5ae4ac03902d678dd14f79667d0b919edf3f5
Juliobs22 <56235190+Juliobs22@users.noreply.github.com>
2024-06-30 14:18:48 -0600

    Add files via upload

---
+++ js/FadeEffect.js
@@ -0,0 +1,10 @@
+function fadeEffect(id) {
+    const $fadeElement = document.getElementById(id);
+    $fadeElement.classList.toggle('f-hide');
+}
+
+class Effects {
+    constructor() {
+    }
+
+}
